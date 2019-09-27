248703b180731d260abc8d1c7624860c428cd01b
Vincent Scavetta <scavet64@students.rowan.edu>
2019-09-27 00:41:46 -0400

    Implement IComparable interface to keep games in order by name

--- NoiseBot/Commands/TextCommands/GameBotCommands/GameListModel.cs
+++ NoiseBot/Commands/TextCommands/GameBotCommands/GameListModel.cs
@@ -1,15 +1,25 @@
 ﻿using Newtonsoft.Json;
+using System;
 
 namespace NoiseBot.Commands.VoiceCommands.GameBotCommands
 {
-    public class GameListModel
+    public class GameListModel : IComparable
     {
+        [JsonProperty("GameName")]
+        public string Name { get; private set; }
+
         public GameListModel(string name)
         {
             Name = name;
         }
 
-        [JsonProperty("GameName")]
-        public string Name { get; private set; }
+        public int CompareTo(object obj)
+        {
+            if (obj is GameListModel other)
+            {
+                return Name.CompareTo(other.Name);
+            }
+            return 0;
+        }
     }
 }
